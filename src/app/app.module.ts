8b433a238d0395e14dc2a5289df1604d88d3fc34
moosabaloch <moosa.bh@gmail.com>
2016-12-31 14:12:41 +0500

    Fire base Authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import {
+  AngularFireModule,
+  AuthMethods,
+  AuthProviders
+} from "angularfire2";
 import { AppComponent } from './app.component';
-
+const firebaseConfig={
+apiKey: "AIzaSyB8k_kjCZIM7bvyrm7RjcLyyqSILGlCVic",
+    authDomain: "fir-auth-63c46.firebaseapp.com",
+    databaseURL: "https://fir-auth-63c46.firebaseio.com",
+    storageBucket: "fir-auth-63c46.appspot.com",
+};
 @NgModule({
   declarations: [
     AppComponent
@@ -12,9 +21,16 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig,{
+      provider:AuthProviders.Google,
+      method:AuthMethods.Popup
+    })
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports:[
+    BrowserModule,
+  ]
 })
 export class AppModule { }
